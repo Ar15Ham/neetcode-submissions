653f1b87c8607eb42f9bd1c71f3ff035736f485e
Ar15Ham <149173734+Ar15Ham@users.noreply.github.com>
2026-04-18 19:51:46 +0100

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+# Longest common prefix:
+# Input:
+# Array of strings: strs
+            
+# Output:         
+# 1) if prefix: "prefix"
+# 2) else: ""
+# 
+# Cases:
+# strs = ['hello'] -> output: "hello"
+# 
+# Example:
+# strs = ['hello', 'he', 'here'] -> output: "he"
+#          i
+# 1) Go through each letter incrementally of the strs[0]
+# 2) Check whether each letter in each string in strs is the same as strs[o]
+# 3) If there is a difference in one of the letters 
+#  OR the length of the current string is equal to the position up to now
+# 4) return the current subsequence
+# 5) If the loop is completed without breaking then the first word is the common subsequence
+
+        if len(strs) == 0:
+            return ""
+        
+        for char in range(len(strs[0])):
+            for string in strs[1:]:
+                if len(string) == char or string[char] != strs[0][char]:
+                    return strs[0][:char]
+        
+        return strs[0]
+
+    
+
+
+    
